b044fafae25dbafe9ac304653f1f4f246020d21c
Thibaud Arend <thibaud.arend@gmail.com>
2013-11-03 09:34:12 +0000

    web.js read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+   var html = fs.readFileSync(htmlfile).toString();
+   response.send(html);
 });
 
 var port = process.env.PORT || 5000;
